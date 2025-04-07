24a5c413c009ea2647ca3e36e9fb46c65eff95d4
devilBoi_YangKhor <ugyendendup@yangkhor.com>
2025-04-07 11:16:53 +0600

    07.04.2025

--- src/app/(calibration_services)/submit-applications/application_form.tsx
+++ src/app/(calibration_services)/submit-applications/application_form.tsx
@@ -476,7 +476,7 @@ const ApplicationSubmitForm = () => {
               <button
                 type="button"
                 className="mt-2 bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600"
-                onClick={() => addEquipment(values, setFieldValue)}
+                onClick={() => addEquipment()}
                 >
                 Add More
               </button>
